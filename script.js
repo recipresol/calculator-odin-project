efcc35e1c9eabc515548808e2628a8d1592adf54
recipresol <93782793+recipresol@users.noreply.github.com>
2025-02-14 00:30:41 -0500

    Add operation button functionality

--- script.js
+++ script.js
@@ -5,30 +5,69 @@ let operations = [
     {key: '/', fn: (a, b) => a / b},
 ]
 
-function operate(operator, a, b) {
-    return operations.filter((op) => op.key = operator)[0](a, b);
+function operate(fn) {
+    b = newEntry ? entry : b;
+    a = fn(a, b);
+    display.textContent = a;
+    entry = 0;
+    newEntry = false;
 }
 
-let a, b = 0;
-let disp = 0;
+let a = 0;
+let b = 0;
+let entry = 0;
+let newEntry = false;
+let currentOp = (a, b) => b;
 
 let display = document.querySelector("#display");
 function addDigit(digit) {
-    disp = 10 * disp + digit;
-    display.textContent = disp;
+    newEntry = true;
+    entry = 10 * entry + digit;
+    display.textContent = entry;
 }
 
 let digitsDiv = document.querySelector("#digits");
 for (let i = 0; i < 10; i++) {
     let digitButton = document.createElement("button");
     digitButton.textContent = i;
-    digitButton.addEventListener("click", () => { addDigit(i) });
+    digitButton.addEventListener("click", () => { 
+        addDigit(i); 
+    });
     digitsDiv.appendChild(digitButton);
 }
 
-let addButton = document.querySelector("#add");
-let subButton = document.querySelector("#sub");
-let mulButton = document.querySelector("#mul");
-let divButton = document.querySelector("#div");
-let clrButton = document.querySelector("#clr");
+let operatorsDiv = document.querySelector("#operators");
+operations.forEach((op) => {
+    let opButton = document.createElement("button");
+    opButton.textContent = op.key;
+    opButton.addEventListener("click", () => {
+        if (newEntry) {
+            operate(currentOp);
+        }
+        currentOp = op.fn;
+    });
+    operatorsDiv.appendChild(opButton);
+});
 
+let eqsButton = document.querySelector("#eqs");
+eqsButton.addEventListener("click", () => {
+    operate(currentOp);
+});
+
+let cleButton = document.querySelector("#cle");
+cleButton.addEventListener("click", () => {
+    if (newEntry) {
+        entry = 0;
+        display.textContent = entry;
+    }
+});
+
+let claButton = document.querySelector("#cla");
+claButton.addEventListener("click", () => {
+    a = 0;
+    b = 0;
+    entry = 0;
+    newEntry = false;
+    currentOp = (a, b) => b;
+    display.textContent = entry;
+});
