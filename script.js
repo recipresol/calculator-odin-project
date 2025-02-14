c8922418cbfda85ad4be5e7d47e6c426aea2d9f5
recipresol <93782793+recipresol@users.noreply.github.com>
2025-02-13 22:25:09 -0500

    Change display logic to not use HTML text

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-let operations = {
-    '+': (a, b) => a + b,
-    '-': (a, b) => a - b,
-    '*': (a, b) => a * b,
-    '/': (a, b) => a / b,
-}
+let operations = [
+    {key: '+', fn: (a, b) => a + b},
+    {key: '-', fn: (a, b) => a - b},
+    {key: '*', fn: (a, b) => a * b},
+    {key: '/', fn: (a, b) => a / b},
+]
 
 function operate(operator, a, b) {
-    return operations[operator](a, b);
+    return operations.filter((op) => op.key = operator)[0](a, b);
 }
+
+let a, b = 0;
+let disp = 0;
 
 let display = document.querySelector("#display");
 function addDigit(digit) {
-    if (display.textContent != "0") {
-        display.textContent += digit;
-    }
-    else {
-        display.textContent = digit;
-    }
+    disp = 10 * disp + digit;
+    display.textContent = disp;
+}
+
+let digitsDiv = document.querySelector("#digits");
+for (let i = 0; i < 10; i++) {
+    let digitButton = document.createElement("button");
+    digitButton.textContent = i;
+    digitButton.addEventListener("click", () => { addDigit(i) });
+    digitsDiv.appendChild(digitButton);
 }
 
 let addButton = document.querySelector("#add");
@@ -25,10 +32,3 @@ let mulButton = document.querySelector("#mul");
 let divButton = document.querySelector("#div");
 let clrButton = document.querySelector("#clr");
 
-let digitsDiv = document.querySelector("#digits");
-for (let i = 0; i < 10; i++) {
-    let digitButton = document.createElement("button");
-    digitButton.textContent = i;
-    digitButton.addEventListener("click", () => { addDigit(parseInt(i)) });
-    digitsDiv.appendChild(digitButton);
-}
