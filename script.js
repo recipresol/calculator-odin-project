721373828a0b34b20d4d384c2e947583fccbb9c4
recipresol <93782793+recipresol@users.noreply.github.com>
2025-02-14 01:55:20 -0500

    Add other buttons and rudimentary interface

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+function format(x) {
+    x = x.toString();
+    return x.slice(0, Math.min(15, x.length));
+}
+
 let operations = [
     {key: '+', fn: (a, b) => a + b},
     {key: '-', fn: (a, b) => a - b},
     {key: '*', fn: (a, b) => a * b},
-    {key: '/', fn: (a, b) => a / b},
+    {key: '/', fn: (a, b) => b != 0 ? a / b : NaN},
 ]
 
 function operate(fn) {
     b = newEntry ? entry : b;
     a = fn(a, b);
-    display.textContent = a;
+    if (isNaN(a)) {
+        allClear("DIV BY 0 ERR");
+        return;
+    }
+    if (a < Number.MIN_SAFE_INTEGER || Number.MAX_SAFE_INTEGER < a) {
+        allClear("SIZE ERR");
+        return;
+    }
+    display.textContent = format(a);
     entry = 0;
     newEntry = false;
 }
@@ -19,16 +32,26 @@ let entry = 0;
 let newEntry = false;
 let currentOp = (a, b) => b;
 
+function allClear(disp) {
+    a = 0;
+    b = 0;
+    entry = 0;
+    newEntry = false;
+    currentOp = (a, b) => b;
+    display.textContent = disp;
+}
+
 let display = document.querySelector("#display");
 function addDigit(digit) {
     newEntry = true;
     entry = 10 * entry + digit;
-    display.textContent = entry;
+    display.textContent = format(entry);
 }
 
 let digitsDiv = document.querySelector("#digits");
-for (let i = 0; i < 10; i++) {
+for (let i = 9; i >= 0 ; i--) {
     let digitButton = document.createElement("button");
+    digitButton.setAttribute("class", "btn")
     digitButton.textContent = i;
     digitButton.addEventListener("click", () => { 
         addDigit(i); 
@@ -39,6 +62,7 @@ for (let i = 0; i < 10; i++) {
 let operatorsDiv = document.querySelector("#operators");
 operations.forEach((op) => {
     let opButton = document.createElement("button");
+    opButton.setAttribute("class", "btn");
     opButton.textContent = op.key;
     opButton.addEventListener("click", () => {
         if (newEntry) {
@@ -58,16 +82,11 @@ let cleButton = document.querySelector("#cle");
 cleButton.addEventListener("click", () => {
     if (newEntry) {
         entry = 0;
-        display.textContent = entry;
+        display.textContent = 0;
     }
 });
 
 let claButton = document.querySelector("#cla");
 claButton.addEventListener("click", () => {
-    a = 0;
-    b = 0;
-    entry = 0;
-    newEntry = false;
-    currentOp = (a, b) => b;
-    display.textContent = entry;
+    allClear(0);
 });
